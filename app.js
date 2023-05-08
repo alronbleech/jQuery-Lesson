30cdbe57c8d2b6a41d35584cc611f12c800e7480
alronbleech <126852030+alronbleech@users.noreply.github.com>
2023-05-08 05:25:15 +0000

    [Finish]Js5

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+//.slideDown();はアニメーションで上から表示
+//.slideUp();下から表示
+//.show();cssで非表示の要素をjQueryで表示
+//.hide();表示されている要素をjQueryで非表示
+$(function(){
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
 });
